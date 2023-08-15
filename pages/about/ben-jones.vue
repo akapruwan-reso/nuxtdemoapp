53263d7937743c32780825a8e2feb573038a8e0a
Anil Kapruwan <akapruwan@gmail.com>
2023-08-15 16:04:07 +1000

    added1

--- pages/about/ben-jones.vue
+++ pages/about/ben-jones.vue
@@ -4,3 +4,11 @@
     <p>This is a nested route, contained in a <code>nuxt</code> tag in the About page</p>
   </div>
 </template>
+<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+<script>
+  hbspt.forms.create({
+    region: "na1",
+    portalId: "42449409",
+    formId: "5a267040-7219-464e-83f9-4a3d9a49f39a"
+  });
+</script>
