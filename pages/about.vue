7dde61a0639af370791b990074c9692fa7e6d285
akapruwan-reso <103974450+akapruwan-reso@users.noreply.github.com>
2023-08-15 16:45:19 +1000

    Update about.vue
    hubspot

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
     <h1>About Page</h1>
-    <p>This is a basic page demonstrating a generated route</p>
+    <p>This is a basic page demonstrating a generated route. 15-Aug</p>
     <br>
     <nuxt />
   </div>
 </template>
+
+<script>
+  export default {
+    mounted() {
+    const script = document.createElement("script");
+    script.src="https://js.hsforms.net/forms/v2.js";
+    document.body.appendChild(script);
+    script.addEventListener("load", () => {
+      if (window.hbspt) {
+        window.hbspt.forms.create({
+          portalId: "42449409",
+          formId: "5a267040-7219-464e-83f9-4a3d9a49f39a",
+          target: "#hubspotForm"
+        })
+      }
+    })
+  }
+}
+</script>
