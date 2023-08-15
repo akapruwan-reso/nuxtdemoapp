dc43fefd595ca0d6e906ef75f8447f131f9cea42
Anil Kapruwan <akapruwan@gmail.com@git config  user.email akapruwan@gmail.com>
2023-08-15 16:12:25 +1000

    added1

--- pages/about/ben-jones.vue
+++ pages/about/ben-jones.vue
@@ -4,11 +4,22 @@
     <p>This is a nested route, contained in a <code>nuxt</code> tag in the About page</p>
   </div>
 </template>
-<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+
 <script>
-  hbspt.forms.create({
-    region: "na1",
-    portalId: "42449409",
-    formId: "5a267040-7219-464e-83f9-4a3d9a49f39a"
-  });
+  export default {
+    mounted() {
+    const script = document.createElement("script");
+    script.src="https://js.hsforms.net/forms/v2.js";
+    document.body.appendChild(script);
+    script.addEventListener("load", () => {
+      if (window.hbspt) {
+        window.hbspt.forms.create({
+          portalId: "42449409",
+          formId: "5a267040-7219-464e-83f9-4a3d9a49f39a",
+          target: "#hubspotForm"
+        })
+      }
+    })
+  }
+}
 </script>
