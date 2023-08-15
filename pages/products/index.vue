5eb8f352633e61cb8aafd8f1a5762fbff2a5b1e8
Anil Kapruwan <akapruwan@gmail.com@git config  user.email akapruwan@gmail.com>
2023-08-15 16:19:19 +1000

    added1

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -4,3 +4,22 @@
     <p>This is another basic page demonstrating a generate route</p>
   </div>
 </template>
+
+<script>
+  export default {
+    mounted() {
+    const script = document.createElement("script");
+    script.src="https://js.hsforms.net/forms/v2.js";
+    document.body.appendChild(script);
+    script.addEventListener("load", () => {
+      if (window.hbspt) {
+        window.hbspt.forms.create({
+          portalId: "42449409",
+          formId: "5a267040-7219-464e-83f9-4a3d9a49f39a",
+          target: "#hubspotForm"
+        })
+      }
+    })
+  }
+}
+</script>
